5c92c5c2e5f7c725e332722b1cb03ad94066644d
nakniner <nakniner@nakniners-iMac.local>
2018-11-26 18:13:06 +0900

    add age expression

---
+++ apostrophe.py
@@ -0,0 +1,14 @@
+import this
+
+message = "One of Python's strenghth is its diverse community"
+print(message)
+
+message = 'One of Python"s strenghth is its diverse community'
+print(message)
+
+test = 2 + 3
+print(test)
+
+age = 40
+message = "My age is " + str(age)
+print(message)
